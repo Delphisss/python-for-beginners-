4b9fe27834a771f7466b792b90fb931875f83135
Delphisss <Kong_led_@hotmail.com>
2023-11-07 14:44:06 +0700

    EP1-4

---
+++ EP3.py
@@ -0,0 +1,13 @@
+#stromg => ข้อความ ตัวอักษร  ' '  " " '''
+#number => integer , float
+#boolen => true , false
+print("Kong D")
+print('Kong S')
+print(30)
+print("0"+"60")
+print(0+60)
+print(-30)
+print(3.99)
+print(True)
+print(False)
+print("lisa \t" + "rose \n" )
